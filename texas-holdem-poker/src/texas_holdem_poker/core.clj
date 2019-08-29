25dfca0b799a297c6016ef36569df4341b6d04e3
phillippavlich <pavlicpm@mcmaster.ca>
2019-08-29 16:54:49 -0400

    converted to main function

--- texas-holdem-poker/src/texas_holdem_poker/core.clj
+++ texas-holdem-poker/src/texas_holdem_poker/core.clj
@@ -2,10 +2,10 @@
 
 (require '[texas-holdem-poker.cards :as cards])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!")
+(defn main
+  "This is the main function to start to the game."
+  (println "Welcome to Phil's Texas Holdem Proker game!")
+  
   (let [deck (cards/build-deck)]
 
     (cards/deal-two-cards deck)
