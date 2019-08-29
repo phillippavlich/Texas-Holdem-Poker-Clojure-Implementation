896149e3ca03fc8a67c442ef54f28a8d658e8669
phillippavlich <pavlicpm@mcmaster.ca>
2019-08-29 16:31:19 -0400

    Build cards and build basic functions to build, shuffle and deal deck

---
+++ texas-holdem-poker/src/texas_holdem_poker/cards.clj
@@ -0,0 +1,21 @@
+(ns texas-holdem-poker.cards)
+
+(def suits [:clubs :spades :hearts :diamonds])
+(def ranks (range 1 14)) ;; range is exclusive
+
+(defn build-deck []
+  "Builds the deck for the first time."
+  (set
+    (for [suit suits
+          rank ranks]
+      {:suit suit :rank rank})))
+
+(defn shuffle-deck
+  "Shuffles the deck."
+  [x]
+  (println (shuffle x) "New shuffled deck!"))
+
+(defn deal-two-cards
+  "Selects 2 cards from the deck x."
+  [x]
+  (println (take 2 x) "These are your two cards!"))
