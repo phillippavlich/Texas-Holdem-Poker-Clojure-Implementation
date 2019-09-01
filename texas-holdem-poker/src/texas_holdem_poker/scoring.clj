e263a27665d8edf29075f67f8bd8c1a4f12f7247
phillippavlich <pavlicpm@mcmaster.ca>
2019-09-01 13:47:51 -0400

    Add number scores for hand combinations

--- texas-holdem-poker/src/texas_holdem_poker/scoring.clj
+++ texas-holdem-poker/src/texas_holdem_poker/scoring.clj
@@ -1,3 +1,15 @@
 (ns texas-holdem-poker.scoring)
 
-
+;;if tied in score rank look to card rank to be a tie breaker!
+;;need a function to look into ties
+(def score-rank
+  {:high-card 0
+   :one-pair 1
+   :two-pair 2
+   :three-of-a-kind 3
+   :straight 4
+   :flush 5
+   :full-house 6
+   :four-of-a-kind 7
+   :straight-flush 8
+   :royal-flush 9})
