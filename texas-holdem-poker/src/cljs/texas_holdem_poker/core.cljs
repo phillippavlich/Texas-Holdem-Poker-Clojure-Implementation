45c33a156f0bf79be285ac198b7422e74c7c620a
phillippavlich <pavlicpm@mcmaster.ca>
2019-09-14 14:46:48 -0400

    adding in rank to card

--- texas-holdem-poker/src/cljs/texas_holdem_poker/core.cljs
+++ texas-holdem-poker/src/cljs/texas_holdem_poker/core.cljs
@@ -30,9 +30,16 @@
 (defn get-suit-url
   "Gets the url for the suit."
   [card]
-  (println (get suit (get (first card) :suit)))
   (get suit (get (first card) :suit))
   )
+
+(defn get-rank
+  "Gets the url for the suit."
+  [card]
+  (println (get (first card) :rank))
+  (get (first card) :rank)
+  )
+
 
 
 (defn greeting []
@@ -41,9 +48,11 @@
    [:p (:text @player-cards)]
    [:div {:class "card"} (:text @flop-view)]
    [:div {:class "card"} (:text @turn-view)
-    [:img {:id "test" :src "hearts.png" :class "suit"}]
+    [:div {:class "left-rank"} (str (get-rank turn))]
+    [:img {:id "test" :src (get-suit-url turn) :class "suit"}]
     ]
    [:div {:class "card"} (:text @river-view)
+    [:div {:class "left-rank"} (str (get-rank river))]
     [:img {:id "test1" :src (get-suit-url river) :class "suit"}]
     ]
    [:img {:id img-id :src "clubs.png" :class "suit"}]
